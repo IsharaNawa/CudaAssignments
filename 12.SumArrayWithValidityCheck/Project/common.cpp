ab9fc98ed0cef9a978d4318229133d11c6809d2c
IsharaNawarathna <e17219@eng.pdn.ac.lk>
2022-12-24 01:46:16 +0530

    added the sum array program

---
+++ 12.SumArrayWithValidityCheck/Project/common.cpp
@@ -0,0 +1,13 @@
+void compare_arrays(int* a, int* b, int size) {
+
+	for (int i = 0;i < size; i++) {
+
+		if (a[i] != b[i]) {
+			printf("Arrays are different \n");
+			return;
+		}
+
+	}
+
+	printf("Arrays are the same \n");
+}
