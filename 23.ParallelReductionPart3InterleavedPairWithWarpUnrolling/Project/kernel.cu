ea22b78b75e4fb30b631ddfdb5c40c8e284e85f5
IsharaNawarathna <e17219@eng.pdn.ac.lk>
2023-01-03 12:36:40 +0530

    added warp divergence

---
+++ 23.ParallelReductionPart3InterleavedPairWithWarpUnrolling/Project/kernel.cu
@@ -0,0 +1,195 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+#include "common.h"
+#include "cuda_common.cuh"
+
+#include "cuda.h"
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+// for initializing arrays
+void initialize(int* input, const int array_size,
+	INIT_PARAM PARAM, int x)
+{
+	if (PARAM == INIT_ONE)
+	{
+		for (int i = 0; i < array_size; i++)
+		{
+			input[i] = 1;
+		}
+	}
+	else if (PARAM == INIT_ONE_TO_TEN)
+	{
+		for (int i = 0; i < array_size; i++)
+		{
+			input[i] = i % 10;
+		}
+	}
+	else if (PARAM == INIT_RANDOM)
+	{
+		time_t t;
+		srand((unsigned)time(&t));
+		for (int i = 0; i < array_size; i++)
+		{
+			input[i] = (int)(rand() & 0xFF);
+		}
+	}
+	else if (PARAM == INIT_FOR_SPARSE_METRICS)
+	{
+		srand(time(NULL));
+		int value;
+		for (int i = 0; i < array_size; i++)
+		{
+			value = rand() % 25;
+			if (value < 5)
+			{
+				input[i] = value;
+			}
+			else
+			{
+				input[i] = 0;
+			}
+		}
+	}
+	else if (PARAM == INIT_0_TO_X)
+	{
+		srand(time(NULL));
+		int value;
+		for (int i = 0; i < array_size; i++)
+		{
+			input[i] = (int)(rand() & 0xFF);
+		}
+	}
+}
+
+int reduction_cpu(int* input, const int size)
+{
+	int sum = 0;
+	for (int i = 0; i < size; i++)
+	{
+		sum += input[i];
+	}
+	return sum;
+}
+
+void compare_results(int gpu_result, int cpu_result)
+{
+	printf("GPU result : %d , CPU result : %d \n",
+		gpu_result, cpu_result);
+
+	if (gpu_result == cpu_result)
+	{
+		printf("GPU and CPU results are same \n");
+		return;
+	}
+
+	printf("GPU and CPU results are different \n");
+}
+
+__global__ void reduction_kernel_warp_unrolling(int* int_array,
+	int* temp_array, int size)
+{
+	int tid = threadIdx.x;
+
+	int index = blockDim.x * blockIdx.x + threadIdx.x;
+
+	int* i_data = int_array + blockDim.x * blockIdx.x;
+
+	// In this implementation we would stop the for loop at 64
+	// if we continue this furthur, divergence would occur
+	for (int offset = blockDim.x / 2; offset >= 64; offset = offset / 2)
+	{
+		if (tid < offset)
+		{
+			i_data[tid] += i_data[tid + offset];
+		}
+		__syncthreads();
+	}
+
+	// now the results are in the first 64 threads
+
+	// now we have to do the itertaions from threads 32 to 1
+
+	// since 32 is considered, all the threads in the first warp is fully occupaid.
+	// therefore no warp divergence there
+	if (tid < 32)
+	{	
+		// now get data into the volatile pointer
+
+		volatile int* vsmem = i_data;
+
+		// then do all the iterations
+		vsmem[tid] += vsmem[tid + 32];
+
+		//the next line will execute once the above execution is over.
+		// therefore the results needed for the next line execution would be
+		// in the array prior to the next line execution.
+
+		vsmem[tid] += vsmem[tid + 16];
+		vsmem[tid] += vsmem[tid + 8];
+		vsmem[tid] += vsmem[tid + 4];
+		vsmem[tid] += vsmem[tid + 2];
+		vsmem[tid] += vsmem[tid + 1];
+	}
+
+	if (tid == 0)
+	{
+		temp_array[blockIdx.x] = i_data[0];
+	}
+}
+
+
+int main(int argc, char ** argv)
+{
+	printf("Running parallel reduction with neighbored pairs improved kernel \n");
+
+	int size = 1 << 27;
+	int byte_size = size * sizeof(int);
+	int block_size = 128;
+
+	int * h_input, *h_ref;
+	h_input = (int*)malloc(byte_size);
+
+ 	initialize(h_input, size, INIT_RANDOM);
+
+	int cpu_result = reduction_cpu(h_input, size);
+
+	dim3 block(block_size);
+	dim3 grid(size / block.x);
+
+	printf("Kernel launch parameters || grid : %d, block : %d \n", grid.x, block.x);
+
+	int temp_array_byte_size = sizeof(int)* grid.x;
+
+	h_ref = (int*)malloc(temp_array_byte_size);
+
+	int * d_input, *d_temp;
+	gpuErrchk(cudaMalloc((void**)&d_input, byte_size));
+	gpuErrchk(cudaMalloc((void**)&d_temp, temp_array_byte_size));
+
+	gpuErrchk(cudaMemset(d_temp, 0, temp_array_byte_size));
+	gpuErrchk(cudaMemcpy(d_input, h_input, byte_size,
+		cudaMemcpyHostToDevice));
+
+	reduction_kernel_warp_unrolling << < grid, block >> > (d_input, d_temp, size);
+
+	gpuErrchk(cudaDeviceSynchronize());
+	gpuErrchk(cudaMemcpy(h_ref, d_temp, temp_array_byte_size, cudaMemcpyDeviceToHost));
+
+	int gpu_result = 0;
+	for (int i = 0; i < grid.x; i++)
+	{
+		gpu_result += h_ref[i];
+	}
+
+	compare_results(gpu_result, cpu_result);
+
+	gpuErrchk(cudaFree(d_input));
+	gpuErrchk(cudaFree(d_temp));
+	free(h_input);
+	free(h_ref);
+
+	gpuErrchk(cudaDeviceReset());
+	return 0;
+}
